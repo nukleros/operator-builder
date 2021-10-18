10e6f7b22817cf57bdcc62c373b741e974f76fc6
Jeff Davis <mr.jefedavis@gmail.com>
2021-10-18 10:49:57 -0400

    ci: correct completion script

--- build/completions.sh
+++ build/completions.sh
@@ -3,5 +3,5 @@ set -e
 rm -rf completions
 mkdir completions
 for sh in bash zsh fish; do
-	go run ./cmd/yot completion "$sh" >"completions/yot.$sh"
+	go run ./cmd/operator-builder completion "$sh" >"completions/operator-builder.$sh"
 done
