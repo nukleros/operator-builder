0f06bf8199e9070257431540a987adc8bab7a39e
Richard Lander <lander2k2@protonmail.com>
2022-10-14 16:50:03 -0400

    feat: add support for darwin/arm64 arch (#59)
    * feat: add support for darwin/arm64 arch
    
    Signed-off-by: Rich Lander <lander2k2@protonmail.com>
    
    * feat: upgrade go version requirement
    
    Signed-off-by: Rich Lander <lander2k2@protonmail.com>
    
    * fix: remove noctx linter
    
    The noctx linter checks for http requests that don't pass in
    `context.Context`.  This project does not have any requests that are in
    any exported packages so we don't need it.
    
    Signed-off-by: Rich Lander <lander2k2@protonmail.com>
    
    * fix: update go version for failing functional tests
    
    Signed-off-by: Rich Lander <lander2k2@protonmail.com>
    
    * ci: update go version for all tests
    
    Signed-off-by: Rich Lander <lander2k2@protonmail.com>
    
    * fix: fix go versions for functional, e2e tests
    
    Signed-off-by: Rich Lander <lander2k2@protonmail.com>
    
    * ci: add testing for go v1.17 back in
    
    Signed-off-by: Rich Lander <lander2k2@protonmail.com>
    
    * ci: remove tests for go version 1.17
    
    Signed-off-by: Rich Lander <lander2k2@protonmail.com>
    
    Signed-off-by: Rich Lander <lander2k2@protonmail.com>

--- pkg/cli/init.go
+++ pkg/cli/init.go
@@ -10,8 +10,10 @@ import (
 	kbcli "sigs.k8s.io/kubebuilder/v3/pkg/cli"
 	cfgv2 "sigs.k8s.io/kubebuilder/v3/pkg/config/v2"
 	cfgv3 "sigs.k8s.io/kubebuilder/v3/pkg/config/v3"
+	"sigs.k8s.io/kubebuilder/v3/pkg/model/stage"
 	"sigs.k8s.io/kubebuilder/v3/pkg/plugin"
 	kustomizecommonv1 "sigs.k8s.io/kubebuilder/v3/pkg/plugins/common/kustomize/v1"
+	kustomizecommonv2alpha "sigs.k8s.io/kubebuilder/v3/pkg/plugins/common/kustomize/v2-alpha"
 	"sigs.k8s.io/kubebuilder/v3/pkg/plugins/golang"
 	declarativev1 "sigs.k8s.io/kubebuilder/v3/pkg/plugins/golang/declarative/v1"
 	golangv2 "sigs.k8s.io/kubebuilder/v3/pkg/plugins/golang/v2"
@@ -33,12 +35,21 @@ func NewKubebuilderCLI() (*kbcli.CLI, error) {
 		workloadv1.Plugin{},
 	)
 
+	gov4Bundle, _ := plugin.NewBundle(golang.DefaultNameQualifier, plugin.Version{Number: 4, Stage: stage.Alpha},
+		licensev1.Plugin{},
+		kustomizecommonv2alpha.Plugin{},
+		configv1.Plugin{},
+		golangv3.Plugin{},
+		workloadv1.Plugin{},
+	)
+
 	c, err := kbcli.New(
 		kbcli.WithCommandName("operator-builder"),
 		kbcli.WithVersion(version),
 		kbcli.WithPlugins(
 			golangv2.Plugin{},
 			gov3Bundle,
+			gov4Bundle,
 			&licensev1.Plugin{},
 			&kustomizecommonv1.Plugin{},
 			&declarativev1.Plugin{},
